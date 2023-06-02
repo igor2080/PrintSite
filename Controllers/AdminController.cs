dd17ff233e4d0fd26994141be58c83fcc87b65a9
igor <igoral2080@gmail.com>
2023-06-02 17:55:29 +0300

    add product creation, deletion

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,10 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
 using PrintSite.Data;
+using PrintSite.Models;
 
 namespace PrintSite.Controllers
 {
-    [Authorize(Roles ="admin")]
+    [Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
         private readonly IStringLocalizer<HomeController> _localizer;
@@ -31,6 +32,41 @@ namespace PrintSite.Controllers
                 product.Visible = !product.Visible;
                 _context.SaveChanges();
             }
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            Product? product = _context.Products.SingleOrDefault(x => x.Id == id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(float price, string description)
+        {
+            List<string> errors = new List<string>();
+            if (price < 0.1)
+                errors.Add("badprice");
+            if (description == null || description.Length < 1)
+                errors.Add("baddescription");
+            if (errors.Count > 0)
+            {
+                return View("CreateProduct", errors);
+            }
+            Product product = new Product() { Description = description, Price = price };
+            _context.Products.Add(product);
+            _context.SaveChanges();
             return RedirectToAction("Index");
         }
     }
